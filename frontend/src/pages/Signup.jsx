25944c0cdbe66701ad2490a6de3f4975573e1a9c
Shivaraj17 <shivarajdhanasekaran836@gmail.com>
2025-03-17 08:47:40 +0530

    I

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -15,7 +15,7 @@ const Signup = () => {
     e.preventDefault();
 
     try {
-      const response = await fetch("https://language-learning-app-1-26rt.onrender.com/signup", {
+      const response = await fetch("http://localhost:5000/api/auth/signup", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(user),
